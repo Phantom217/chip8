17fb4d07f9397728b41784a7f783bccd1000c13f
Tyler Oalman <tyleroalman@gmail.com>
2020-05-31 15:25:34 -0500

    Stub out the cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
+use clap::{AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+pub struct Args {
+    /// Start with debugging info
+    #[clap(short = "D", long)]
+    debug: bool,
+    /// The rom to use
+    rom: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if let Err(e) = run(args) {
+        eprintln!("error: {}", e);
+    }
+}
+
+fn run(args: Args) -> io::Result<()> {
+    todo!()
 }
