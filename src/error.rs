edd61185ac7e9705f7b43f548645f1dbf2c36af6
Tyler Oalman <tyleroalman@gmail.com>
2020-06-03 17:16:38 -0500

    Refactor error::Chip8Error

--- src/error.rs
+++ src/error.rs
@@ -9,18 +9,25 @@ pub enum Chip8Error {
     Io(io::Error),
 }
 
-impl error::Error for Chip8Error {}
-
-impl From<io::Error> for Chip8Error {
-    fn from(error: io::Error) -> Self {
-        Self::Io(error)
-    }
-}
-
 impl fmt::Display for Chip8Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
+            // this is a wrapper, so defer to the underlying types impl of `fmt`
             Self::Io(ref e) => e.fmt(f),
         }
+    }
+}
+
+impl error::Error for Chip8Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            Self::Io(ref e) => Some(e),
+        }
+    }
+}
+
+impl From<io::Error> for Chip8Error {
+    fn from(err: io::Error) -> Self {
+        Self::Io(err)
     }
 }
